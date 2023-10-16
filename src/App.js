ffcb569d33e91644ba1191d22a094b3764c40ad9
Ana Dib <anaflavia.rdib@gmail.com>
2023-10-15 21:57:29 -0300

    feat: Create Netflix clone

--- src/App.js
+++ src/App.js
@@ -1,73 +1,78 @@
-import React, { useEffect, useState } from 'react';
-import { GlobalStyle } from './components/GlobalStyle'
-import { Header, MovieRow, FeatureMovie }  from './components';
+import React, { useEffect, useState } from "react";
+import { GlobalStyle } from "./GlobalStyle";
+import { Header, MovieRow, FeatureMovie } from "./components";
 
-import Tmdb from './Tmdb'
+import Tmdb from "./Tmdb";
 
-import * as S from './style';
+import * as S from "./style";
 
 function App() {
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
-  const [blackHeader, setBlackeader] =useState(false)
+  const [blackHeader, setBlackeader] = useState(false);
 
-  useEffect(() => { // Quando a tela for carregada o react irá executar a função que estiver aqui
+  useEffect(() => {
     const loadAll = async () => {
-      // Pegando a lista TOTAL
-      let list = await Tmdb.getHomeList()
-      setMovieList(list)
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
 
-      // Pegando o filme em destaque
-      let originals = list.filter(item => item.slug === "originals");
-      let randomChosen = Math.floor(Math.random() * (originals[0].item.results.length - 1)); // Math.random() --> número de 0 até 1. Utilizamos o -1 porque o array vai de 0 até 19, sendo que o tamanho é 20.
+      let originals = list.filter((item) => item.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].item.results.length - 1)
+      );
       let chosen = originals[0].item.results[randomChosen];
-      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
-      setFeaturedData(chosenInfo)
-    }
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
+      setFeaturedData(chosenInfo);
+    };
 
     loadAll();
   }, []);
 
-    useEffect(() => {
-      const scrollListener = () => {
-        if(window.scrollY > 10 ) {
-          setBlackeader(true)
-        } else {
-          setBlackeader(false)
-        }
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackeader(true);
+      } else {
+        setBlackeader(false);
       }
+    };
 
-      window.addEventListener('scroll', scrollListener);
+    window.addEventListener("scroll", scrollListener);
 
-      return () => {
-        window.removeEventListener('scroll', scrollListener);
-      } 
-    }, []); 
+    return () => {
+      window.removeEventListener("scroll", scrollListener);
+    };
+  }, []);
 
   return (
     <>
       <GlobalStyle />
       <S.Page>
-        <Header black={blackHeader}/>
-        {featuredData &&
-          <FeatureMovie item={featuredData}/>
-        }
+        <Header black={blackHeader} />
+        {featuredData && <FeatureMovie item={featuredData} />}
         <S.Lists>
           {movieList.map((item, key) => (
             <MovieRow key={key} title={item.title} items={item.item} />
           ))}
         </S.Lists>
         <footer>
-          Feito com <span rol="img" aria-label="coração">❤️</span> pela Ana Flávia Dib. <br />
+          Feito com{" "}
+          <span rol="img" aria-label="coração">
+            ❤️
+          </span>{" "}
+          pela Ana Flávia Dib. <br />
           Direitos de imagem para Netflix <br />
           Dados pegos do site Themoviedb.org <br />
         </footer>
 
-        {movieList.length <= 0 &&
+        {movieList.length <= 0 && (
           <S.Loading>
-            <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando"/>
+            <img
+              src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif"
+              alt="Carregando"
+            />
           </S.Loading>
-        }
+        )}
       </S.Page>
     </>
   );
--- src/components/FeatureMovie/style.js
+++ src/components/FeatureMovie/style.js
@@ -1,119 +1,120 @@
 import styled from "styled-components";
 
 export const Featured = styled.section`
-    height: 100vh;
-    background-image: url(${props => (`https://image.tmdb.org/t/p/original${props.posterPath}`)});
-    background-size: cover;
+  height: 100vh;
+  background-image: url(${(props) =>
+    `https://image.tmdb.org/t/p/original${props.posterPath}`});
+  background-size: cover;
 
-    @media (max-width: 760px) {
-        height: 90vh;
-    }
+  @media (max-width: 760px) {
+    height: 90vh;
+  }
 `;
 
 export const FeaturedVertical = styled.div`
-    width: inherit;
-    height: inherit;
-    background: linear-gradient(to top, #111 10%, transparent 90%);
+  width: inherit;
+  height: inherit;
+  background: linear-gradient(to top, #111 10%, transparent 90%);
 `;
 
 export const FeaturedHorizontal = styled.div`
-    width: inherit;
-    height: inherit;
-    background: linear-gradient(to right, #111 30%, transparent 70%);
-    display: flex;
-    align-items: center;
-    padding-bottom: 150px;
-    padding-top: 70px;
+  width: inherit;
+  height: inherit;
+  background: linear-gradient(to right, #111 30%, transparent 70%);
+  display: flex;
+  align-items: center;
+  padding-bottom: 150px;
+  padding-top: 70px;
 `;
 
 export const MovieInformation = styled.div`
-    max-width: 40%;
-    margin-left: 30px;
+  max-width: 45%;
+  margin-left: 30px;
+
+  h1 {
+    font-size: 60px;
+    font-weight: bold;
+    margin: 0;
+  }
+
+  p {
+    font-size: 20px;
+    color: #999;
+
+    &:nth-child(6) {
+      font-size: 18px;
+      margin: 0;
+    }
+  }
+
+  button {
+    margin-bottom: 18px;
+
+    &:first-of-type {
+      background-color: #fff;
+      color: #000;
+    }
+
+    &:last-of-type {
+      background-color: #333;
+      color: #fff;
+    }
+  }
+
+  @media (max-width: 760px) {
+    max-width: unset;
+    flex-wrap: wrap;
+    margin-right: 30px;
 
     h1 {
-        font-size: 60px;
-        font-weight: bold;
-        margin: 0;
+      font-size: 40px;
     }
 
     p {
-        font-size: 20px;
-        color: #999;
+      font-size: 14px;
 
-        &:nth-child(6) {
-            font-size: 18px;
-            margin: 0;
-        }
+      &:nth-child(6) {
+        font-size: 14px;
+      }
     }
-
-    button {
-        margin-bottom: 18px;
-
-        &:first-of-type {
-            background-color: #fff;
-            color: #000;
-        }
-    
-        &:last-of-type {
-            background-color: #333;
-            color: #fff;
-        }
-    }
-
-    @media (max-width: 760px) {
-        max-width: unset;
-        flex-wrap: wrap;
-        margin-right: 30px;
-
-        h1 {
-            font-size: 40px;
-        }
-
-        p {
-            font-size: 14px;
-
-            &:nth-child(6) {
-                font-size: 14px;
-            }
-        }
-    }
+  }
 `;
 
 export const InfosWrapper = styled.div`
-    display: flex;
+  display: flex;
 
-    p {
-        padding-right: 15px;
-        font-weight: bold;
-        font-size: 18px;
-        margin: 1em 0 0 0;
-        color: #fff;
-        
-        &:first-child {
-            color: #46d369;
-        }
-
-        @media (max-width: 760px) {
-            font-size: 16px;
-        }
-    }
-`;
-
-export const Button = styled.button`
-    border: none;
-    padding: 12px 25px;
-    font-size: 20px;
-    margin-right: 10px;
-    border-radius: 5px;
+  p {
+    padding-right: 15px;
     font-weight: bold;
-    transition: all ease 0.2s;
+    font-size: 18px;
+    margin: 1em 0 0 0;
+    color: #fff;
 
-    &:hover {
-        cursor: pointer;
-        opacity: 70%;
+    &:first-child {
+      color: #46d369;
     }
 
     @media (max-width: 760px) {
-        font-size: 16px;
+      font-size: 16px;
     }
+  }
+`;
+
+export const Button = styled.button`
+  border: none;
+  padding: 12px 25px;
+  font-size: 20px;
+  margin-right: 10px;
+  border-radius: 5px;
+  font-weight: bold;
+  transition: all ease 0.2s;
+
+  &:hover {
+    cursor: pointer;
+    opacity: 70%;
+  }
+
+  @media (max-width: 760px) {
+    font-size: 16px;
+  }
 `;
